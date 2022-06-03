13a3c7a2d0f61ed57383f4e17035da627ef5e2ad
Codemode001 <haroldrex12331@gmail.com>
2022-06-03 13:19:57 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Test from "./test";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Test} />
-    </Switch>
-  </BrowserRouter>
-);
+root.render(<Test />);
